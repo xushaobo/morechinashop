39a806bebf249944956641b31da86fc4ca96835e
xushaobo <xushaobo333@gmail.com>
2019-11-01 09:44:28 +0800

    商品模型

--- app/Admin/Controllers/UsersController.php
+++ app/Admin/Controllers/UsersController.php
@@ -10,7 +10,7 @@ use Encore\Admin\Grid;
 use Encore\Admin\Layout\Content;
 use Encore\Admin\Show;
 
-class UserController extends Controller
+class UsersController extends Controller
 {
     use HasResourceActions;
 
@@ -77,18 +77,35 @@ class UserController extends Controller
      *
      * @return Grid
      */
+
+    protected $title = '用户';
+
     protected function grid()
     {
         $grid = new Grid(new User);
 
-        $grid->id('Id');
-        $grid->name('Name');
-        $grid->email('Email');
-        $grid->email_verified_at('Email verified at');
-        $grid->password('Password');
-        $grid->remember_token('Remember token');
-        $grid->created_at('Created at');
-        $grid->updated_at('Updated at');
+        //创建一个列名为ID的列，内容是用户的id字段
+        $grid->id('ID');
+        //创建一个列名为用户名的列，内容是用户的name字段
+        $grid->name('用户名');
+        //创建一个列名为的列，内容是用户的email字段
+        $grid->email('邮箱');
+        $grid->email_verified_at('已验证邮箱')->display(function ($value) {
+            return $value ? '是':'否';
+        });
+        $grid->created_at('注册时间');
+
+        //不在页面显示 ’新建‘ 按钮，因为不需要在后台新建用户
+        $grid->disableCreateButton();
+        //同时在每一行也不显示'编辑'按钮
+        $grid->disableActions();
+
+        $grid->tools(function ($tools) {
+            // 禁用批量删除按钮
+            $tools->batch(function ($batch){
+                $batch->disableDelete();
+            });
+        });
 
         return $grid;
     }

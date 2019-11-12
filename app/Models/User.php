a85758e3140a510a233a51578210abb6efcd748c
xushaobo <xushaobo333@gmail.com>
2019-11-12 22:46:30 +0800

    收货地址列表

--- app/Models/User.php
+++ app/Models/User.php
@@ -48,4 +48,9 @@ class User extends Authenticatable implements MustVerifyEmail
     {
         return $this->hasMany(CartItem::class);
     }
+
+    public function addresses()
+    {
+        return $this->hasMany(UserAddress::class);
+    }
 }
